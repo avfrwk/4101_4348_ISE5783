429d8a41f179df2544f61de87fa6c031bb0f46e7
aviad <avfrwk@gmail.com>
2023-04-30 17:58:09 +0300

    start of targil 5

---
+++ src/lighting/AmbientLight.java
@@ -0,0 +1,14 @@
+package lighting;
+
+import primitives.Color;
+import primitives.Double3;
+
+/**this class represent the ambient light in the scene*/
+public class AmbientLight {
+    private Color intensity;
+
+    public AmbientLight(Color IA,Double3 KA){
+
+    }
+
+}
