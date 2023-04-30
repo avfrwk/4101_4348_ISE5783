f9a677f7fd7f1c022a8e952d4374e373499b208b
יאשיהו <yoshi@yatmal.co.il>
2023-04-30 22:37:09 +0300

    AmbientLight

--- src/lighting/AmbientLight.java
+++ src/lighting/AmbientLight.java
@@ -6,9 +6,17 @@ import primitives.Double3;
 /**this class represent the ambient light in the scene*/
 public class AmbientLight {
     private Color intensity;
+    public static AmbientLight NONE=new AmbientLight(Color.BLACK,Double3.ZERO);
 
     public AmbientLight(Color IA,Double3 KA){
-
+        this.intensity=new Color(IA.scale(KA));
     }
 
+    public AmbientLight(double KA){
+        this.intensity=new Color().scale(KA);
+    }
+
+    public Color getIntensity() {
+        return intensity;
+    }
 }
